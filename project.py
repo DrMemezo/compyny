b2109113f709ba1b9aa5224a729d9cb1aa78faad
DrMemezo <nyanisbusy@gmail.com>
2024-04-15 12:44:53 +0500

    added a slow_print procedure, to dramitaclly display text to the terminal

--- project.py
+++ project.py
@@ -1,9 +1,24 @@
+from time import sleep
 
-# LETHAL (PROTO) COM-PY-NY BY JAMAL
+
+# LETHAL (PROTO) COM-PY-NY
 
 def main():
     print("MOON - DESCRIPTION")
+    #sleep(2)
+    print("Some event")
+    slow_print(input())
 
+    action = "generic"
+    print(f"You did this {action}")
+
+
+def slow_print(message:str, wait:float=0.1, end:str="\n"):
+    """NOTE: Only pass strings!!"""
+    for char in message:
+        print(char, end="", flush=True)
+        sleep(wait)
+    print(end=end)    
 
 if __name__ == "__main__":
     main()

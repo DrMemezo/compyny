144ad0fd053152ed656b453c4bbbd35593dc1dfe
DrMemezo <nyanisbusy@gmail.com>
2024-05-22 21:24:30 +0500

    lots of things... added a ship class. more maybe

--- classes.py
+++ classes.py
@@ -2,17 +2,20 @@ import random
 
 
 
-class Ship:
-    pass
-
-
 class Event:
     """Event occurs when the player progresses to another room
         They have a rarity type, and the probability of an event occuring is linked to that type"""
-    
+
     def __init__(self, rarity:str) -> None:
         if rarity not in ["common", "uncommon", "rare"]:
             raise ValueError("unsupported rarity")
+        self.hidden:bool = random.choice([True, False])
+
+    def is_hidden(self) -> bool:
+        return self.hidden
+
+    def option(self, action:str):
+        """Calls a function based on the action performed by the user"""
         pass
 
 class Scrap(Event):
@@ -27,20 +30,96 @@ class Scrap(Event):
             case 'uncommon':
                 self.value =  random.randint(135, 185)
                 self.name = random.choice(["Candlestick","Airhorn","Rubiks Cube"])
+        
             case 'common':
                 self.value = random.randint(50, 100)
                 self.name = random.choice(["Fake rolex watch","May/June 2024 Maths paper","disapproving skull"])
-    
+
+
     def __str__(self) -> str:
         return random.choice([
             f"Despite the overwhelming darkness... you find a {self.name}",
             f"A {self.name} blocks your path...",
-            f"How peculiar... a {self.name} lies in front of you..."
+            f"How peculiar... a {self.name} lies in front of you...",
+            f"You were so distracted by the blood on the wall... you didn't even realise the {self.name}",
+            f"You stumbled over a {self.name}, How clumsy!",
+            f"In the corner of your eye, you spotted a {self.name}"
         ])
 
-    def inspect(self) -> str:
-        return f"{self.name.title()} has a value of {self.value}"
+    def option(self, action: str):
+        action = action.strip().lower()
     
     def unique_event():
         """If any scrap is unique from the rest, they can use this module"""
         pass
+
+class Ship:
+    def __init__(self, name:str="Johnathan doe") -> None:
+        self.health = 3
+        self.scraps = 0
+        self.name = name
+    
+    def __add__(self, scrap:Scrap):
+        return self.scraps + scrap.value
+
+
+def inspect(events:list[Event]) -> str:
+    message = ""
+    for event in events:
+        if type(event) is Scrap and not event.is_hidden():
+            message += f"{event.name.title()} has a value of {event.value}\n"
+    return message
+
+def find(events:list[Event]) -> str:
+    message = ""
+    for event in events:
+        try:
+            if event.is_hidden():
+                message += f"You Found a {event.name}\n"
+        except:
+            pass
+    if not message:
+        message = "Nothing of value was found..."
+
+    return message
+
+def think(*args) -> str:
+    message = "You think long and hard about what to do next...\n"
+    message += "'find': Find and locate any hidden items (or dangers) in this room\n"
+    message += "'inspect': Inspect scrap that you've already found\n"
+    message += "'progress': Progress to the next room\n"
+
+    return message
+
+def collect(events:list[Event]) -> str:
+    for event in events:
+        if event.isinstance(Scrap):
+            pass
+            
+
+
+OPTIONS = {
+    'inspect' : inspect,
+    'find' : find,
+    'think' : think
+}
+
+# Stores the style for each action
+styles = {
+    "default" : "bold white on blue",
+    "danger" : "bold white on red",
+    "info": "turquoise2",
+    "inspect": "sea_green1",
+    "find": "light_slate_blue",
+    "think": "bold spring_green3 on white",
+    "advise": "grey50"
+}
+
+# Stores the interval for slow printing for each action
+wait = {
+    "think": 0.001,
+    "advise": 0.02,
+    "find": 0.03,
+    "inspect": 0.05,
+    "overview": 0.025
+}
--- project.py
+++ project.py
@@ -3,24 +3,32 @@ from time import sleep
 import csv
 import random
 from rich.console import Console
-import classes
-
+import classes 
+from classes import styles, wait, Ship
 # Console class from rich, to make text prettier to look at.
 CONSOLE = Console()
-styles = {
-    "default" : "bold white on blue",
-    "danger" : "bold white on dark red",
-    "info": "bold white on green"
-}
-
 
 def main():
+    # Game's title card
+    # UNDEVELOPED: titlecard()
+    
+    # Initialising the ship:
+    # DISABLED: main_ship:Ship = ship_init()
     # Intro to the moon, a spooky description, etc. 
     # DISABLED: print_intro()
     # Play state
-    play()
+    play(main_ship)
     # End state
-    end()
+    end(main_ship)
+
+def ship_init() -> Ship:
+    
+    
+    slow_print("WHAT IS YOUR NAME,", wait=0.2, style=styles['default'])
+    slow_print("T R A V E L L E R ?", wait=0.2, style=styles['danger'])
+    name = input().strip()
+    return Ship(name=name)
+    
 
 def print_intro():
     intro:dict = get_intro()
@@ -47,24 +55,43 @@ def slow_print(message:str, wait:float=0.1,
         sleep(wait)
     print(end=end)    
 
-def play():
+
+def play(ship:Ship):
     global CONSOLE
     alive:bool = True 
     while alive:
-        events:list = get_events()
+        # Show progression into the next area
+        next_room()
+        events:list = get_events() # Gets a list of events
         for event in events:
             # Print event info
-            slow_print(event.__str__(), wait=0.05)
+            if not event.is_hidden():
+                slow_print(event.__str__(), wait=wait['overview'])
 
         # Prompt user for input
-        slow_print("What to do now?", style=styles['info'], wait=0.02)
-        action = input()
+        slow_print("What to do now?", style=styles['info'], wait=wait['advise'])
+        action = input().strip().lower()
+                
+        while action != "progress":
+            try:
+                slow_print(classes.OPTIONS[action](events), wait=wait[action], style=styles[action])
+            except KeyError:
+                CONSOLE.print("psst... it maybe helpful to.. THINK.. about your actions...",
+                              style=styles['advise'])
+            finally:
+                action = input().strip()
 
-        # Print out user action
-        CONSOLE.print(f"You did this {action}")
-        
         # See if user lives or dies
         alive = random.choice([True, False])
+
+def next_room():
+    global CONSOLE
+    message = random.choice(["Walking through the maze of endless hallways, you find the next room.",
+                             "You feel a cold chill up your spine as you walk into the next room",
+                             "You hear sounds and scrapes in the distance as you walk into the next room",
+                             "Your flashlight flickers in the ominous darkness, yet you find your way to the next room."])
+    
+    slow_print(message,wait=0.05, style="bold bright_cyan")
 
 def get_events() -> list:
     """Returns a list of event objects"""
@@ -85,12 +112,13 @@ def get_events() -> list:
     
     return events
 
-def end(dead:bool=True):
+def end(ship:Ship, dead:bool=True):
     style:str = "green" 
     if dead:
         style = "bold white on red"
     
-    slow_print("GAME OVER", style=style)
+    slow_print("GAME OVER", style=styles['default'])
+    slow_print(f"Points: {ship.scraps}", style=styles['default'])
 
 if __name__ == "__main__":
     main()
